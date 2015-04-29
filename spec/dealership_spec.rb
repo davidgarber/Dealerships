29e3b4189b600a4e389aec811193853b68b364ac
chase & david <student@epicodus.com>
2015-04-29 09:22:43 -0700

    write initial files, write returns the name of the dealership spec, pass

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,15 @@
+require('rspec')
+require('dealership')
+
+describe(Dealership) do
+#
+# end
+
+describe('#name') do
+  it("returns the name of the dealership") do
+    test_dealership = Dealership.new("Bob's Used Cars")
+    expect(test_dealership.name()).to(eq("Bob's Used Cars"))
+  end
+end
+
+end
