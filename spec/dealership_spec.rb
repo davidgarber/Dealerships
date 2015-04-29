d1bcd717d79969f875200694bfffebb88f154a56
chase & david <student@epicodus.com>
2015-04-29 11:53:44 -0700

    initially returns an empty array of the the cars for the dealership

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -5,17 +5,22 @@ describe(Dealership) do
 #
 # end
 
-describe('#name') do
-  it("returns the name of the dealership") do
-    test_dealership = Dealership.new("Bob's Used Cars")
-    expect(test_dealership.name()).to(eq("Bob's Used Cars"))
+  describe('#name') do
+    it("returns the name of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.name()).to(eq("Bob's Used Cars"))
+    end
   end
-end
-describe('#id') do
-  it("returns the id of the dealership") do
-    test_dealership = Dealership.new("Bob's Used Cars")
-    expect(test_dealership.id()).to(eq(1))
-end
-end
-
+  describe('#id') do
+    it("returns the id of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+  describe('#cars') do
+    it("initially return an empty array of the cars for the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.cars()).to(eq([]))
+    end
+  end
 end
